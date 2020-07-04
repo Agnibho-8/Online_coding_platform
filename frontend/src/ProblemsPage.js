8577bce37367cc21ce232003a9b6d3fcc2692e52
unknown <contact.agnibho@gmail.com>
2020-07-05 00:25:09 +0530

    UI updates

--- frontend/src/ProblemsPage.js
+++ frontend/src/ProblemsPage.js
@@ -1,48 +1,72 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link, Redirect, NavLink } from "react-router-dom";
 
 export class ProblemsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            problemId:5,
-            problemStatement:'',
-            difficultyLevel:'',
-            pointsAlotted:'',
-            sampleInput:'',
-            sampleOutput:''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      problemId: [],
+      problemStatement: [],
+      difficultyLevel: [],
+      pointsAlotted: [],
+      sampleInput: "",
+      sampleOutput: "",
     };
+  }
 
-    componentDidMount() {
-        axios.get('http://localhost:8080/problems')
-            .then( (response)=> {
-                // handle success
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/problems/")
+      .then((response) => {
+        // handle success
 
-                this.setState({
-                    problemId: response.data[0].problemId,
-            problemStatement:response.data[0].problemStatement,
-            difficultyLevel:response.data[0].difficultyLevel,
-            pointsAlotted:response.data[0].pointsAlotted,
-            sampleInput:response.data[0].sampleInput,
-            sampleOutput:response.data[0].sampleOutput
-                })
-                console.log(response);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-    }
-    render() {
-        return (
+        this.setState({
+          problemId: response.data,
+          problemStatement: response.data,
+        });
+
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  }
+  onClick = (event) => {
+    this.state.x = event.currentTarget.textContent;
+
+    console.log(this.state.x);
+  };
+  Shodata = () => {
+    var data = this.state.problemId.map((problem) => {
+      return (
+        <NavLink
+          to={{
+            pathname: "/solution",
+            state: { problemID: problem.problemId },
+          }}
+        >
+          <div onClick={this.onClick}>
+            <h3>{problem.problemStatement}</h3>
             <div>
-                
-        <h1>{this.state.problemId}</h1>
-        <div>{this.state.problemStatement}</div>
+              {problem.difficultyLevel}, Maximum points: {problem.pointsAlotted}
             </div>
-        )
-    }
+          </div>
+        </NavLink>
+      );
+    });
+    return data;
+  };
+
+  render() {
+    var data = this.Shodata();
+    return (
+      <div>
+        <div>{data}</div>
+      </div>
+    );
+  }
 }
 
-export default ProblemsPage
+export default ProblemsPage;
--- frontend/src/SolutionPage.js
+++ frontend/src/SolutionPage.js
@@ -1,124 +1,135 @@
-import React,{Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Image,Container,Dropdown,Button,Form} from 'react-bootstrap'
-import style from 'styled-components'
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Image, Container, Dropdown, Button, Form } from "react-bootstrap";
+import style from "styled-components";
+import ProblemsPage from "./ProblemsPage";
 
-const Header=style.div`
+const Header = style.div`
 width:100%;
 border-bottom:#bf0000 3px solid;
-text-align:center;`
+text-align:center;`;
 
-
-const DropdownMenu=style(Dropdown)`
+const DropdownMenu = style(Dropdown)`
 padding:0px 30px 0px 0px;
 
-`
+`;
 
-const FormInput=style(Form)`
+const FormInput = style(Form)`
 padding:30px 30px 0px 0px;
-`
- class solutionPage extends Component {
-  constructor(props){
-     super(props);
-     this.state={
-      language:"",
-       postrequestdata:[],
-       code:""
-       
-
-     }
+`;
+class solutionPage extends ProblemsPage {
+  constructor(props) {
+    super(props);
+    this.state = {
+      language: "",
+      postrequestdata: [],
+      code: "",
+    };
   }
-//   componentDidMount(){
-//     Axios.post(`http://localhost:8080/users/searchbydate?date=2020-12-10${this.state.name}`).then(
-//       response=>{
-//         const UserDetails=response.data;
-        
-//           this.setState({
-//             details:UserDetails
-//           })
-        
-//       }
-//     );
-    
-//   }
-  handleInput=(event)=>{
+  //   componentDidMount(){
+  //     Axios.post(`http://localhost:8080/users/searchbydate?date=2020-12-10${this.state.name}`).then(
+  //       response=>{
+  //         const UserDetails=response.data;
+
+  //           this.setState({
+  //             details:UserDetails
+  //           })
+
+  //       }
+  //     );
+
+  //   }
+  handleInput = (event) => {
     this.setState({
-
-      code:event.target.value
-     
-    })
-    
-  }
-  buttonClick=()=>{
+      code: event.target.value,
+    });
+  };
+  buttonClick = () => {
     console.log("hERE");
-    var solution={
-      "language":this.state.language,
-      "answer":this.state.code
+    var solution = {
+      language: this.state.language,
+      answer: this.state.code,
+    };
+    const id = this.props.location.state.problemID;
+    Axios.post(`http://localhost:8080/problems/${id}`, solution)
+      .then((responce) => {
+        console.log(responce);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  handleClick = (event) => {
+    console.log(event);
+    var option = "Java";
+    if (event === "1") option = "Python";
+    else if (event === "2") {
+      option = "C";
     }
-    console.log(solution)
-    Axios.post(`http://localhost:8080/problems/2`,solution)
-    .then(responce=>{console.log(responce)})
-    .catch(error=>console.log(error))
-  }
-
-
-  handleClick=(event)=>{
-    console.log(event)
-    var option="Java"
-    if(event==="1")
-   
-    option="Python"
-    else if(event==="2")
-    {
-    option="C"}
-   
 
     this.setState({
-
-      language:option
-     
-    })
-
-
-  }
-   render(){
+      language: option,
+    });
+  };
+  render() {
     return (
       <div>
-      <img src="./header.jpg" width="100%" height="100px"></img> 
-      <Container>
-     
-        <p style={{padding:"100px 0 10px 0", margin:"0px"}}><strong>Select Language</strong></p>
-        <DropdownMenu>
-        <Dropdown.Toggle variant="primary" id="dropdown-basic" >
-         {this.state.language.length!==0?this.state.language:"Language Dropdown"} 
-        </Dropdown.Toggle>
-   
-        <Dropdown.Menu>
-          <Dropdown.Item eventKey="1" onSelect={this.handleClick}>Pyhton</Dropdown.Item>
-          <Dropdown.Item eventKey="2" onSelect={this.handleClick}>C</Dropdown.Item>
-          <Dropdown.Item eventKey="3" onSelect={this.handleClick}>Java</Dropdown.Item>
-        </Dropdown.Menu>
-      </DropdownMenu>
+        <img src="./header.jpg" width="100%" height="100px"></img>
+        <Container>
+          <p style={{ padding: "100px 0 10px 0", margin: "0px" }}>
+            <strong>Select Language</strong>
+          </p>
+          <DropdownMenu>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              {this.state.language.length !== 0
+                ? this.state.language
+                : "Language Dropdown"}
+            </Dropdown.Toggle>
 
-      <FormInput>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label><strong>Start Coding:</strong></Form.Label>
-    <Form.Control type="text" as="textarea" onChange={this.handleInput} style={{border:"2px #bf0000 solid",height:"600px"}}/>
-    
-  </Form.Group>
-  </FormInput>
-  <Button variant="primary" size="lg" disabled={this.state.language.length!=0&&this.state.code.length!=0?false:true} onClick={this.buttonClick} block>
-    Submit
-  </Button>
- 
-    </Container>
-    </div>
-     );
-      
- 
+            <Dropdown.Menu>
+              <Dropdown.Item eventKey="1" onSelect={this.handleClick}>
+                Pyhton
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="2" onSelect={this.handleClick}>
+                C
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="3" onSelect={this.handleClick}>
+                Java
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </DropdownMenu>
+
+          <FormInput>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>
+                <strong>Start Coding:</strong>
+              </Form.Label>
+              <Form.Control
+                type="text"
+                as="textarea"
+                onChange={this.handleInput}
+                style={{ border: "2px #bf0000 solid", height: "600px" }}
+              />
+            </Form.Group>
+          </FormInput>
+          <Button
+            variant="primary"
+            size="lg"
+            disabled={
+              this.state.language.length !== 0 && this.state.code.length !== 0
+                ? false
+                : true
+            }
+            onClick={this.buttonClick}
+            block
+          >
+            Submit
+          </Button>
+        </Container>
+      </div>
+    );
+  }
 }
- }
 export default solutionPage;
